9495436b734dd9ead1a659c99ba6caacf05dfd0f
Asher <141028690+No-bodyq@users.noreply.github.com>
2025-12-31 16:03:01 +0100

    hide

--- assessment_engine/settings.py
+++ assessment_engine/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-x)qng0q&w2)@s3l@^nb@q6itk-sku541od&dilw=5tn-x$9h6g
 GRADING_STRATEGY = os.getenv('GRADING_STRATEGY', 'mock')
 
 # LLM API Keys
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', 'AIzaSyA0u-vFuK__ywnoV7kGUWPBQKJvid_0AW4')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
